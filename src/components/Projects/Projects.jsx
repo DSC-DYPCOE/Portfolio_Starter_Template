77fe96a1e63d400d36b87e493685b8b0246c5c1f
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-04-12 23:58:41 +0530

    starter code updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import classes from "./Projects.module.css";
+// import Card from "../Card/Card";
+// import { ProjectsData } from "../../assets/projectsData";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <>
+      <h1>Projects</h1>
+      <div className={classes.project_section}>
+        {/* Render our cards using map here. */}
+      </div>
+    </>
+  );
 };
 
 export default Projects;
